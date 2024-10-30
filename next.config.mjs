1d3077f146fdc776a46d5601a7f1adc81be858ce
zerotower69 <zerotower@163.com>
2024-10-30 18:18:21 +0800

    workflow: 添加github工作流

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output:"export",
+    images:{
+        //github pages 无法对图像优化
+        unoptimized:true
+    },
+    basePath:"/react-components",
+    assetPrefix:"/react-components"
+};
 
 export default nextConfig;
